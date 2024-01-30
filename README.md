8825c8ab0b97e91dee7276d3be7c6857f762187d
Julie Vandenberghe <julie.vandenberghe.59@gmail.com>
2024-01-30 11:47:45 +0100

    💄 add sass files + 📝 form in html + 📚 update readme

---
+++ README.md
@@ -0,0 +1,8 @@
+# Clean3000
+
+
+## Pour compiler tous les fichiers sass en css
+sass sass:css 
+
+## Pour compiler automatiquement le sass à l'enregistrement
+sass --watch sass/main.scss css/main.css
---
+++ js/report.js
@@ -0,0 +1,10 @@
+//CLASSE
+export default class Report {
+    // Propriétés
+    constructor(company, date, comment) { 
+        this.company = company;
+        this.date = date;
+        this.comment = comment;
+    }
+
+};
