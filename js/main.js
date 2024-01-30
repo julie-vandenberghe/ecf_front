592bc5fcfa94c29d3ab6b68bab74a88271f2db30
Julie Vandenberghe <julie.vandenberghe.59@gmail.com>
2024-01-30 22:04:05 +0100

    ✨ display details (after create) + 🐛 bug on delete + 💄 footer fixed to the bottom + 💄 header color + 🐛 solved via W3C validator + :recycle: burger menu imported in main + :recycle: multiple if transformed into a switch

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 import Report from "./report.js";
 
+import './burgerMenu.js';
 
 // TRAITEMENT DU FORMULAIRE POUR AJOUTER UN AVIS
 let form = document.getElementById("newReport");
@@ -16,12 +17,26 @@ form.addEventListener("submit", (e) => {
     let date = new Date(dateInput).toLocaleDateString('fr-FR'); // 2023-02-01 sera affiché 01/01/2023
     let comment = document.getElementById("comment").value;
 
+    switch (company) {
+        case 'company-1':
+            company = "Jardins d'Ariana";
+            break;
+        case 'company-2':
+            company = "Jérôme Livran";
+            break;
+        case 'company-3':
+            company = "Philippe Parguey";
+            break;
+        case 'company-4':
+            company = "Archimed";
+            break;
+        case 'company-5':
+            company = "BeCom";
+            break;
+        default:
+            break;
+    }
 
-    if (company == "company-1") {company = "Jardins d'Ariana"}
-    if (company == "company-2") {company = "Jérôme Livran"}
-    if (company == "company-3") {company = "Philippe Parguey"}
-    if (company == "company-4") {company = "Archimed"}
-    if (company == "company-5") {company = "BeCom"}
 
     if (comment == "") {comment = "Pas d'observations"}
 
@@ -34,22 +49,28 @@ form.addEventListener("submit", (e) => {
     }
     // S'ils ne sont pas vides, on ajoute le nouvel avis
     else {
-    reports.push(new Report(company, date, comment));
-    window.localStorage.setItem('reports', JSON.stringify(reports));
-    console.log(reports);
-    document.getElementById('message').innerHTML = '<i class="fa-solid fa-circle-info"></i> Avis ajout&eacute;';
-    form.reset(); // Permet de vider le formulaire 
-    displayReportsList(); // Sans cet appel, la liste des avis sur la page web ne serait pas mise à jour dynamiquement afin de matcher les changements dans les données.
-
-}});
+        const report = new Report(company, date, comment)
+        
+        reports.push(report);
+        window.localStorage.setItem('reports', JSON.stringify(reports));
+        
+        console.log(reports);
+        
+        document.getElementById('message').innerHTML = '<i class="fa-solid fa-circle-info"></i> Avis ajout&eacute;';
+        form.reset(); // Permet de vider le formulaire 
+        
+        displayReportsList(); // Sans cet appel, la liste des avis sur la page web ne serait pas mise à jour dynamiquement afin de matcher les changements dans les données.
+        displayDetails(report);
+    }
+});
 
 
 // TRAITEMENT DU FORMULAIRE POUR RECHERCHER UN AVIS (ET LE SUPPRIMER)
 let formCheckout = document.getElementById("deleteReport");
 formCheckout.addEventListener("submit", (e) => {
     e.preventDefault(); //Annule le comportement par défait. Ici, cela permet de ne pas recharger la page
-    let recordNumber = document.getElementById("recordNumber").value;    
-    
+    let recordNumber = Number(document.getElementById("recordNumber").value);
+
     // On cherche le client dans nos données
     let findOne = reports.find(function(report){
         return (report.id  === recordNumber);
@@ -58,8 +79,8 @@ formCheckout.addEventListener("submit", (e) => {
     if (findOne) {
         let div = `<div><i class="fa-solid fa-circle-info"></i> Avis ${findOne.id} supprimé</div>`;
         document.getElementById('message').innerHTML = div;
-        reports = reports.filter(item => {
-            return (item.nom  !== recordNumber); //Permet de ne supprimer que le client recherché. Si on enlève le !, cela supprime TOUUUUS les reports, sauf celui recherché.
+        reports = reports.filter(report => {
+            return (report.id  !== recordNumber); //Permet de ne supprimer que le client recherché. Si on enlève le !, cela supprime TOUUUUS les reports, sauf celui recherché.
         });
         window.localStorage.setItem('reports', JSON.stringify(reports));
     }
@@ -102,7 +123,19 @@ function displayReportsList() {
 }
 
 
-// FONCTION POUR AFFICHER L'AVIS NOUVELLEMENT CRÉé
+// FONCTION POUR AFFICHER L'AVIS NOUVELLEMENT CRÉÉ
+function displayDetails(report) {
+    let reportItem = document.createElement("div");
+    reportItem.classList.add("avis");
+    reportItem.innerHTML = `<div class="avisTitle">Avis de passage n° ${report.id} </div><div class="avisCompany"> ${report.company} </div><div class="avisDate"> ${report.date}</div><div class="avisComment"> ${report.comment}</div>`;
+    document.querySelector('#detailsRecord').appendChild(reportItem);
+
+    document.querySelector('#landing').classList.add('hidden')
+    document.querySelector('#new').classList.add('hidden')
+    document.querySelector('#delete').classList.add('hidden')
+    document.querySelector('#list').classList.add('hidden')
+    document.querySelector('#details').classList.remove('hidden')
+}
 
 
 // PERMET LE DISPLAY D'UNE SECTION EN FONCTION DE CE QUI EST CLIQUÉ DANS LE MENU
@@ -111,6 +144,8 @@ document.querySelector('#newLink').addEventListener('click', function() {
     document.querySelector('#new').classList.remove('hidden')
     document.querySelector('#delete').classList.add('hidden')
     document.querySelector('#list').classList.add('hidden')
+    document.querySelector('#details').classList.add('hidden')
+
     document.getElementById('message').innerHTML = ''
 })
 
@@ -119,6 +154,8 @@ document.querySelector('#deleteLink').addEventListener('click', function() {
     document.querySelector('#new').classList.add('hidden')
     document.querySelector('#delete').classList.remove('hidden')
     document.querySelector('#list').classList.add('hidden')
+    document.querySelector('#details').classList.add('hidden')
+
     document.getElementById('message').innerHTML = ''
 })
 
@@ -127,5 +164,7 @@ document.querySelector('#listLink').addEventListener('click', function() {
     document.querySelector('#new').classList.add('hidden')
     document.querySelector('#delete').classList.add('hidden')
     document.querySelector('#list').classList.remove('hidden')
+    document.querySelector('#details').classList.add('hidden')
+
     document.getElementById('message').innerHTML = ''
 })
